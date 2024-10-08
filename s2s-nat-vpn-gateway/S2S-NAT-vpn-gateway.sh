f78f282c6090b56208d8086a120850f3356663e0
wshamroukh <wshamroukh@gmail.com>
2024-10-08 14:53:07 +0400

    Optimizing the code

--- s2s-nat-vpn-gateway/S2S-NAT-vpn-gateway.sh
+++ s2s-nat-vpn-gateway/S2S-NAT-vpn-gateway.sh
@@ -312,8 +312,8 @@ case "${PLUTO_VERB}" in
         ip tunnel add "${VTI_IF}" local "${PLUTO_ME}" remote "${PLUTO_PEER}" mode vti \
             okey "${PLUTO_MARK_OUT%%/*}" ikey "${PLUTO_MARK_IN%%/*}"
         ip link set "${VTI_IF}" up
-        ip route add $hub1_nat_address dev "${VTI_IF}"
-        ip route add $onprem2_nat_address dev "${VTI_IF}"
+        ip route add ${hub1_nat_address} dev "${VTI_IF}"
+        ip route add ${onprem2_nat_address} dev "${VTI_IF}"
         sysctl -w "net.ipv4.conf.${VTI_IF}.disable_policy=1"
         ;;
     down-client)
@@ -321,10 +321,6 @@ case "${PLUTO_VERB}" in
         ;;
 esac
 EOT
-
-sed -i "s,\\\$hub1_nat_address,${hub1_nat_address}," $ipsec_vti_file
-sed -i "s,\\\$onprem2_nat_address,${onprem2_nat_address}," $ipsec_vti_file
-
 
 ##### copy files to onprem gw
 echo -e "\e[1;36mCopying and applying S2S VPN Config files to $onprem1_vnet_name-gw Gateway VM...\e[0m"
@@ -402,8 +398,8 @@ case "${PLUTO_VERB}" in
         ip tunnel add "${VTI_IF}" local "${PLUTO_ME}" remote "${PLUTO_PEER}" mode vti \
             okey "${PLUTO_MARK_OUT%%/*}" ikey "${PLUTO_MARK_IN%%/*}"
         ip link set "${VTI_IF}" up
-        ip route add $hub1_nat_address dev "${VTI_IF}"
-        ip route add $onprem1_nat_address dev "${VTI_IF}"
+        ip route add ${hub1_nat_address} dev "${VTI_IF}"
+        ip route add ${onprem1_nat_address} dev "${VTI_IF}"
         sysctl -w "net.ipv4.conf.${VTI_IF}.disable_policy=1"
         ;;
     down-client)
@@ -411,10 +407,6 @@ case "${PLUTO_VERB}" in
         ;;
 esac
 EOT
-
-sed -i "s,\\\$hub1_nat_address,${hub1_nat_address}," $ipsec_vti_file
-sed -i "s,\\\$onprem1_nat_address,${onprem1_nat_address}," $ipsec_vti_file
-
 
 ##### copy files to onprem gw
 echo -e "\e[1;36mCopying and applying S2S VPN Config files to $onprem2_vnet_name-gw Gateway VM...\e[0m"
