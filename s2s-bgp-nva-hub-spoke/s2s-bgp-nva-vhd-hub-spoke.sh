37c6f536b75d88b4eb71b5749fb64311ca8bda5c
wshamroukh <wshamroukh@gmail.com>
2025-05-10 20:54:38 +0400

    check connectivity from spoke1 and hub1 to rest of network

--- s2s-bgp-nva-hub-spoke/s2s-bgp-nva-vhd-hub-spoke.sh
+++ s2s-bgp-nva-hub-spoke/s2s-bgp-nva-vhd-hub-spoke.sh
@@ -765,9 +765,16 @@ ssh -n -o BatchMode=yes -o StrictHostKeyChecking=no $onprem2_gw_pubip "ping -c 3
 ssh -n -o BatchMode=yes -o StrictHostKeyChecking=no $onprem2_gw_pubip "ping -c 3 $spoke2_vm_ip"
 ssh -n -o BatchMode=yes -o StrictHostKeyChecking=no $onprem2_gw_pubip "ping -c 3 $onprem1_vm_ip"
 
+echo -e "\e[1;36mChecking connectivity from $hub1_vm VM to the rest of network topology...\e[0m"
+ssh -n -o BatchMode=yes -o StrictHostKeyChecking=no $onprem2_gw_pubip "ssh -n -o BatchMode=yes -o StrictHostKeyChecking=no $hub1_vm_ip 'ping -c 3 $onprem1_vm_ip'"
+ssh -n -o BatchMode=yes -o StrictHostKeyChecking=no $onprem2_gw_pubip "ssh -n -o BatchMode=yes -o StrictHostKeyChecking=no $hub1_vm_ip 'ping -c 3 $onprem2_vm_ip'"
+
+echo -e "\e[1;36mChecking connectivity from $spoke1_vm VM to the rest of network topology...\e[0m"
+ssh -n -o BatchMode=yes -o StrictHostKeyChecking=no $onprem2_gw_pubip "ssh -n -o BatchMode=yes -o StrictHostKeyChecking=no $spoke1_vm_ip 'ping -c 3 $onprem1_vm_ip'"
+ssh -n -o BatchMode=yes -o StrictHostKeyChecking=no $onprem2_gw_pubip "ssh -n -o BatchMode=yes -o StrictHostKeyChecking=no $spoke1_vm_ip 'ping -c 3 $onprem2_vm_ip'"
+
 echo -e "\e[1;35m$hub1_vnet_name-fw VM is now up. You can access it by going to https://$hub1_fw_public_ip/ \n usename: root \n passwd: opnsense\nIt's highly recommended to change the password\e[0m"
 echo -e "\e[1;35mYou can also ssh root@$hub1_fw_public_ip\nPassword: opnsense\e[0m"
-echo -e "\e[1;35mTo test connectivity, connect to onprem1 Gateway VM $onprem1_gw_pubip via ssh and from there, check connectivity to and from hub1 vm $hub1_vm_ip, spoke1 vm $spoke1_vm_ip and spoke2 vm $spoke2_vm_ip....\e[0m"
 
 #cleanup
 # az group delete -g $rg -y --no-wait
